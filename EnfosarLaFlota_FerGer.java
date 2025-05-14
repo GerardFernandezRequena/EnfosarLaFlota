0a3b07941a1a59321040e1a43406c6ee67afc101
GeroFer43 <104083414+GerardFernandezRequena@users.noreply.github.com>
2025-05-15 01:37:55 +0200

    Update EnfosarLaFlota_FerGer.java

--- EnfosarLaFlota_FerGer.java
+++ EnfosarLaFlota_FerGer.java
@@ -1,235 +1,221 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class EnfosarLaFlota_FerGer {
+public class HundirLaFlota {
 
-	static int Mida = 8;
+    private static final int TAMANO_TABLERO = 8;
+    private static final int NUM_BARCOS = 5;
+    private static final Scanner scanner = new Scanner(System.in);
 
-	public static Scanner teclattext = new Scanner (System.in);
-	public static Scanner teclatnum = new Scanner (System.in);
+    public static void main(String[] args) {
+        System.out.println("¡BIENVENIDO A HUNDIR LA FLOTA!\n");
 
+        // Configuración de jugadores
+        String jugador1 = obtenerNombreJugador(1);
+        String jugador2 = obtenerNombreJugador(2);
 
-	public static String DemanarNomDelJugador(int jugador){
-	   	  String NomDelJugador;
-	      System.out.print("Entra el Nom del Jugador "+jugador+": ");
-	      NomDelJugador = teclattext.nextLine();
-	      return NomDelJugador;
-	}
+        // Inicialización de tableros
+        char[][] tableroJugador1 = crearTablero();
+        char[][] tableroJugador2 = crearTablero();
 
-	//Omplir El Joc Amb El Mar
-	public static void OmplirLaMatriuAmbMar(char[][] MatriuUsuari){
-	      for (int fila = 0; fila < MatriuUsuari.length; fila++) {
-	         for (int columna = 0; columna < MatriuUsuari[fila].length;columna++) {
-	        	 		MatriuUsuari[fila][columna] = '~';
-	         }
-	      }
-	   }
+        // Posicionamiento de barcos
+        posicionarBarcos(jugador1, tableroJugador1);
+        posicionarBarcos(jugador2, tableroJugador2);
 
-   //Posar El vaixells
-   public static void posarVaixells(char joc[][], String posicio) {
-	   		String separar [] = posicio.split(",");
-			int posicion1 = Integer.parseInt(separar[0]) - 1;
-			int posicion2 = Integer.parseInt(separar[1]) - 1;
-			joc[posicion1][posicion2] = 'v';
-   }
+        // Juego principal
+        jugarPartida(jugador1, jugador2, tableroJugador1, tableroJugador2);
 
-   //Per Llançar Els Misils a Els Vaixells
-	public static void llensarMisil (char joc[][], String posicio) {
+        scanner.close();
+    }
 
-		String separar [] = posicio.split(",");
-		int posicio1 = Integer.parseInt(separar[0]) - 1;
-		int posicio2 = Integer.parseInt(separar[1]) - 1;
-		if (joc[posicio1][posicio2] == 'v') {
-			System.out.println("El Misil Ha Tocat Un Vaixell!!!!!");
-			joc[posicio1][posicio2] = 'm';
-		} else {
-			System.out.println("El Misil No ha Tocat Cap Vaixell!!!!");
-		}
-	}
+    private static String obtenerNombreJugador(int numeroJugador) {
+        System.out.print("Introduce el nombre del Jugador " + numeroJugador + ": ");
+        return scanner.nextLine();
+    }
 
-	// Comproba si el Numeros Introdüit esta en El Rang
-	public static boolean comprovaPosicions(String posicio, char joc[][], boolean esPosicio) {
+    private static char[][] crearTablero() {
+        char[][] tablero = new char[TAMANO_TABLERO][TAMANO_TABLERO];
+        for (int i = 0; i < TAMANO_TABLERO; i++) {
+            for (int j = 0; j < TAMANO_TABLERO; j++) {
+                tablero[i][j] = '~'; // '~' representa agua
+            }
+        }
+        return tablero;
+    }
 
-		boolean posicioCorrecta = true;
-			try {
+    private static void posicionarBarcos(String nombreJugador, char[][] tablero) {
+        System.out.println("\n" + nombreJugador + ", es hora de posicionar tus barcos:");
+        mostrarTablero(tablero, false);
 
-				String parts [] = posicio.split(",");
-				int posicion1 = Integer.parseInt(parts[0]);
-				int posicion2 = Integer.parseInt(parts[1]);
+        for (int i = 1; i <= NUM_BARCOS; i++) {
+            boolean posicionValida;
+            do {
+                System.out.println("Posición del barco " + i + " de " + NUM_BARCOS);
+                System.out.print("Ingresa coordenadas (fila,columna) entre 1-" + TAMANO_TABLERO + ": ");
+                String coordenadas = scanner.nextLine();
 
-				if (posicion1 < 1 || posicion1 < 1) {
-						System.out.println("Una de les Posicions Té un Nombre Més Petit del Demanat");
-						System.out.println("Posa Un Altre"+"\n");
-						posicioCorrecta = false;
-				} else if ( posicion1 > Mida || posicion2 > Mida ) {
-					System.out.println("Una de les Posicions té un nombre Més Gran del Demanat");
-					System.out.println("Posa Un Altre"+"\n");
-					posicioCorrecta = false;
-		
-				}
-				
-				if ( joc [posicion1 - 1][posicion2 - 1] == 'v' && esPosicio == true) {
-					posicioCorrecta = false;
-					System.out.println("En la Posició Ja Hi Ha un vaixell");
-					System.out.println("Prova amb una altra Posició."+"\n");
-				}
-	
-				} catch (NumberFormatException e) {
-					
-					System.out.println("No Hi ha Coma o Hi Ha un Espai");
-					System.out.println("Torna a Posar Amb el Format Demanat."+"\n");
-					posicioCorrecta = false;
-				
-				} catch (ArrayIndexOutOfBoundsException e) {
-					System.out.println("Només S'ha Detectat un Nombre");
-					System.out.println("Torna a Posar"+"\n");
-					posicioCorrecta = false;
-				}
-	
-				return posicioCorrecta;
-	}
-	
-	//Per Mostrar La Matriu
-	public static void mostraMatriu(char joc [][]) {
-		for (int fila = 0; fila < joc.length; fila++) {
-			for (int colum = 0; colum < joc[fila].length;colum++) {
-				System.out.print(joc[fila][colum]+ " ");
-			}
-			System.out.println("");
-		}
-		System.out.println("");
-	}
+                posicionValida = validarCoordenadas(coordenadas, tablero, true);
 
-	// Si Ha enfonsat Tots Els Vaixells Per Acabar La Partida.
-	public static boolean haGuanyat (char joc [][]) {
-				
-				boolean guanyaLaPartida = false;			
-				int comptador = 0;
+                if (posicionValida) {
+                    String[] partes = coordenadas.split(",");
+                    int fila = Integer.parseInt(partes[0]) - 1;
+                    int columna = Integer.parseInt(partes[1]) - 1;
+                    tablero[fila][columna] = 'B'; // 'B' representa barco
+                    mostrarTablero(tablero, false);
+                }
+            } while (!posicionValida);
+        }
+        System.out.println("¡Todos los barcos han sido posicionados!\n");
+    }
 
-				for (int fila = 0; fila < joc.length; fila++) {
-						for (int colum = 0; colum < joc[fila].length; colum++) {
-							if (joc[fila][colum] == 'v') {
-								comptador++;
-						}
-					}
-				}
-				if (comptador == 0) {
-					guanyaLaPartida = true;
-				}
-			return guanyaLaPartida;
-		}
-	
-	// Comprova Quin Juagador té més Vaixells per veure qui ha Guanya
-	public static int comprovaGuanyador(char joc [][]) {
-			int comptadorvaixells = 0;
-				for (int fila = 0; fila < joc.length; fila++) {
-					for (int colum = 0; colum < joc[fila].length; colum++) {
-						if (joc[fila][colum] == 'v') {
-							comptadorvaixells++;
-						}
-					}
-				}
-				return comptadorvaixells;
-			}
+    private static boolean validarCoordenadas(String coordenadas, char[][] tablero, boolean esPosicionamiento) {
+        try {
+            String[] partes = coordenadas.split(",");
+            if (partes.length != 2) {
+                throw new IllegalArgumentException();
+            }
 
-public static void main(String[] args) {
-		
-		String jugador1, jugador2;
-		char joc1 [][] = new char [Mida][Mida];
-		char joc2 [][] = new char [Mida][Mida];
-		String posicio;
-		boolean posicioCorrecta;
-		
-		//Posar Noms a Els Jugadors
-		jugador1 = DemanarNomDelJugador(1);
-		jugador2 = DemanarNomDelJugador(2);
-		
-		//Omplim La Matriu amb m (Mar)
-		OmplirLaMatriuAmbMar(joc1);
-		OmplirLaMatriuAmbMar(joc2);
+            int fila = Integer.parseInt(partes[0]);
+            int columna = Integer.parseInt(partes[1]);
 
-		System.out.println("Posicionament Del Jugador "+jugador1);
-		for (int i = 0; i < 5; i++) {
-			do {
-				System.out.println ("Posicio del vaixell " +(i + 1));
-				System.out.println("Posa La Posició on Vols Posar El Vaixell. (Exemples: 0,0 | 1,5 | 5,6)");
-				System.out.println("La Posició tenen que Ser (màxim "+Mida+"): ");
-				posicio = teclattext.nextLine();
-				posicioCorrecta = comprovaPosicions(posicio, joc1, true);
-			
-			} while (posicioCorrecta == false);
-		
-			posarVaixells(joc1, posicio);
-		}
+            if (fila < 1 || fila > TAMANO_TABLERO || columna < 1 || columna > TAMANO_TABLERO) {
+                System.out.println("Error: Las coordenadas deben estar entre 1 y " + TAMANO_TABLERO);
+                return false;
+            }
 
-		System.out.println("Posicionament Del Jugador "+jugador2);
-		for (int i = 0; i < 5; i++) {
-			do {
-					System.out.println("Posicio del vaixell "+(i + 1));
-					System.out.println("Posa La Posició on Vols Posar El Vaixell. (Exemples: 0,0 | 1,5 | 5,6 )");
-					System.out.println("La posició tenen que Ser"+" (màxim "+Mida+"): ");
-					posicio = teclattext.nextLine();
-					posicioCorrecta = comprovaPosicions(posicio, joc2, true);
-				} while (posicioCorrecta == false);
-				
-			posarVaixells(joc2, posicio);
-		}
+            if (esPosicionamiento && tablero[fila - 1][columna - 1] == 'B') {
+                System.out.println("Error: Ya hay un barco en esa posición");
+                return false;
+            }
 
-		
-		System.out.println("\n"+"Comença La Partida!"+"\n");
-		
-		// Llançar Els Misils Fins a 5 Cops
-		for (int i = 0; i < 5; i++) {
-				do {
-						System.out.println((i+1)+" Tirada "+" del "+jugador1+"\n");
-						System.out.println("Tauler de "+jugador2);
-						mostraMatriu(joc2);
-						System.out.println("Posa la Posició per Atacar El Vaixell de "+jugador2+" (Exemples: 0,0 | 1,5 | 5,6 )");
-						System.out.println("La posició tenen que Ser"+" (màxim "+Mida+"): ");
-						posicio = teclattext.nextLine();
-						posicioCorrecta = comprovaPosicions(posicio, joc1, false);
-					} while (posicioCorrecta == false);
-				llensarMisil(joc2, posicio);
-				boolean guanyaLaPartida = haGuanyat(joc2);
-				
-				if (guanyaLaPartida == true) {
-					break;
-				}
-	
-				do {
-						System.out.println((i+1)+" Tirada "+" del "+jugador2+"\n");
-						System.out.println("Tauler de "+jugador1);
-						mostraMatriu(joc1);
-						System.out.println("Posa la Posició per Atacar El Vaixell de "+jugador1+" (Exemples: 0,0 | 1,5 | 5,6 )");
-						System.out.println("La posició tenen que Ser"+" (màxim "+Mida+"): ");
-						posicio = teclattext.nextLine();
-						posicioCorrecta = comprovaPosicions(posicio, joc1, false);
-					} while (posicioCorrecta == false);
-						llensarMisil(joc1, posicio);
-						guanyaLaPartida = haGuanyat(joc1);
-				
-				if (guanyaLaPartida == true) {
-					break;
-				}
-			
-		}
-			
-			// Determina Qui ha Guanyat.
-			
-			System.out.println("\n"+"La Partida Ha Acabat!!!!"+"\n");
-			
-			int comptadorvaixells1,comptadorvaixells2;
-			
-			comptadorvaixells1 = comprovaGuanyador(joc1);
-			comptadorvaixells2 = comprovaGuanyador(joc2);
-			
-			if (comptadorvaixells1 > comptadorvaixells2) {
-				System.out.println("El "+jugador1+" Ha Guanyat Amb " + comptadorvaixells1 + " Vaixells!");
-			} else if (comptadorvaixells1 < comptadorvaixells2) {
-				System.out.println("El "+jugador2+" Ha Guanyat Amb " + comptadorvaixells2 + " Vaixells!");
-			} else {
-				System.out.println("Hi Ha Empat Amb " + comptadorvaixells1 + " Vaixells!");
-			}
-		
-	}
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Error: Formato incorrecto. Usa fila,columna (ej: 3,5)");
+            return false;
+        }
+    }
+
+    private static void jugarPartida(String jugador1, String jugador2, 
+                                   char[][] tablero1, char[][] tablero2) {
+        System.out.println("\n¡COMIENZA LA PARTIDA!\n");
+        boolean juegoTerminado = false;
+        int turno = 0;
+
+        while (!juegoTerminado && turno < NUM_BARCOS * 2) {
+            // Turno del jugador 1
+            juegoTerminado = realizarTurno(jugador1, jugador2, tablero2);
+            if (juegoTerminado) {
+                System.out.println("\n¡" + jugador1 + " ha ganado la partida!");
+                break;
+            }
+
+            // Turno del jugador 2
+            juegoTerminado = realizarTurno(jugador2, jugador1, tablero1);
+            if (juegoTerminado) {
+                System.out.println("\n¡" + jugador2 + " ha ganado la partida!");
+                break;
+            }
+
+            turno++;
+        }
+
+        if (!juegoTerminado) {
+            determinarGanadorPorPuntos(jugador1, jugador2, tablero1, tablero2);
+        }
+    }
+
+    private static boolean realizarTurno(String atacante, String defensor, char[][] tablero) {
+        System.out.println("\nTurno de " + atacante + " (atacando a " + defensor + ")");
+        mostrarTablero(tablero, true);
+
+        boolean disparoValido;
+        int fila = 0, columna = 0;
+
+        do {
+            System.out.print("Ingresa coordenadas para disparar (fila,columna): ");
+            String coordenadas = scanner.nextLine();
+            disparoValido = validarCoordenadas(coordenadas, tablero, false);
+
+            if (disparoValido) {
+                String[] partes = coordenadas.split(",");
+                fila = Integer.parseInt(partes[0]) - 1;
+                columna = Integer.parseInt(partes[1]) - 1;
+            }
+        } while (!disparoValido);
+
+        if (tablero[fila][columna] == 'B') {
+            System.out.println("¡IMPACTO! Has hundido un barco.");
+            tablero[fila][columna] = 'X'; // 'X' representa barco hundido
+            return !quedanBarcos(tablero);
+        } else {
+            System.out.println("¡AGUA! No había ningún barco.");
+            if (tablero[fila][columna] != 'X') {
+                tablero[fila][columna] = 'O'; // 'O' representa disparo al agua
+            }
+            return false;
+        }
+    }
+
+    private static boolean quedanBarcos(char[][] tablero) {
+        for (char[] fila : tablero) {
+            for (char celda : fila) {
+                if (celda == 'B') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void determinarGanadorPorPuntos(String jugador1, String jugador2, 
+                                                  char[][] tablero1, char[][] tablero2) {
+        int barcosJugador1 = contarBarcosRestantes(tablero1);
+        int barcosJugador2 = contarBarcosRestantes(tablero2);
+
+        System.out.println("\n¡Se han agotado los turnos!");
+        System.out.println("Barcos restantes de " + jugador1 + ": " + barcosJugador1);
+        System.out.println("Barcos restantes de " + jugador2 + ": " + barcosJugador2);
+
+        if (barcosJugador1 > barcosJugador2) {
+            System.out.println("¡" + jugador1 + " gana por tener más barcos restantes!");
+        } else if (barcosJugador2 > barcosJugador1) {
+            System.out.println("¡" + jugador2 + " gana por tener más barcos restantes!");
+        } else {
+            System.out.println("¡Empate! Ambos jugadores tienen la misma cantidad de barcos.");
+        }
+    }
+
+    private static int contarBarcosRestantes(char[][] tablero) {
+        int contador = 0;
+        for (char[] fila : tablero) {
+            for (char celda : fila) {
+                if (celda == 'B') {
+                    contador++;
+                }
+            }
+        }
+        return contador;
+    }
+
+    private static void mostrarTablero(char[][] tablero, boolean esAtaque) {
+        System.out.print("  ");
+        for (int i = 1; i <= TAMANO_TABLERO; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < TAMANO_TABLERO; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < TAMANO_TABLERO; j++) {
+                char celda = tablero[i][j];
+                if (esAtaque && celda == 'B') {
+                    System.out.print("~ "); // No mostrar barcos enemigos
+                } else {
+                    System.out.print(celda + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
-	
